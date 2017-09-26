5bef8fc867fc428723929a481c53291a6ff70838
chakravarthy <chakravarthy@proinfnet.com>
2017-09-26 21:51:01 +0530

    new

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,7 @@ public class Helloworld{
 
     public static void main(String[] args){
 
-        for (int i=1;i<=10;i++){
+        for (int i=1;i<=5;i++){
             System.out.println("Helloworld..."+i);
         }
     }
